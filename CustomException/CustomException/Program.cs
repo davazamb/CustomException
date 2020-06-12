4144c203c9b2e178507790c6a78e4623ce1fbfc9
davaz <davazamb10@outlook.com>
2020-06-12 13:17:59 -0400

    example custom exception

--- CustomException/CustomException/Program.cs
+++ CustomException/CustomException/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace CustomException
 {
-    class Program
+    //Custom User defiened Exception Created here, using Exception as base class
+    public class TestCustomException : Exception
     {
-        static void Main(string[] args)
+
+        public TestCustomException(string message) : base(message)
         {
+            this.HelpLink = "for more information https://github.com/davazamb";
+            this.Source = "This is source of Error";
+        }
+
+    }
+
+    //Usual Class with method Show(), which throws Exception with message
+    public class MyClass
+    {
+        public static void Show()
+        {
+            throw new TestCustomException("This is Custom Exception example in C#");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //enclose method inside try
+            try
+            {
+                MyClass.Show();
+            }
+            //catch User defined Exception
+            catch (TestCustomException ex)
+            {
+                //Show Custom defined Error messages
+                Console.WriteLine("Error Message:-" + ex.Message);
+                Console.WriteLine("Hyper Link :-" + ex.HelpLink);
+                Console.WriteLine("Source :- " + ex.Source);
+                Console.ReadKey();
+
+            }
         }
     }
 }
--- CustomException/CustomException/Properties/AssemblyInfo.cs
+++ CustomException/CustomException/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CustomException")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/davazamb")]
 [assembly: AssemblyProduct("CustomException")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
